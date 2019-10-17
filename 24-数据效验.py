91d30f7192884c59d374d1ac89b78ec8b0059c59
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2019-10-17 16:38:45 +0800

    Update 24-数据效验.py

--- 24-数据效验.py
+++ 24-数据效验.py
@@ -17,3 +17,17 @@ data = pd.read_excel("pd操作.xlsx")
 # print(data)
 # datafrome有两个轴，0表示从上到下，1表示从左到右，因为咱们要比较一行一行的数据，所以设置1
 data.apply(score_validation, axis=1)
+
+
+
+# 数据分割
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_excel("pd操作.xlsx")
+#  data['Name']获取到的是一个序列，再转str字符串，再变换
+df = data['Name'].str.split(expand=True)
+data['Fname'] = df[0]
+data['Lname'] = df[1]
+print(data)
