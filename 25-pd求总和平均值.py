c8fd7cc07f1a61ba8b05b7dfdfec985b24c13d28
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2019-10-17 17:34:58 +0800

    Create 25-pd求总和平均值.py

---
+++ 25-pd求总和平均值.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 求和求平均值
+data = pd.read_excel("pd操作.xlsx", sheet_name="Cprice", index_col="ID")
+# 读取指定列的值
+temp = data[["Score1", "Score2", "Score3"]]
+# 求出每一行的和
+row_sum = temp.sum(axis=1)
+# 求出每一行的平均值,使用平均值是mean
+row_avg = temp.sum(axis=1)
+
+# 将求出来的总和和平均值数据添加到原表中
+data['Sum'] = row_sum
+data['Avg'] = row_avg
+
+col_mean = data[["Score1", "Score2", "Score3", "Sum", "Avg"]].mean()
+data = data.append(col_mean, ignore_index=True)
+
+print(data)
