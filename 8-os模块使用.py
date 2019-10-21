2a81cd7f94dbbbe17acbde7029d500c67d0804a5
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2019-10-21 13:55:22 +0800

    Update 8-os模块使用.py

--- 8-os模块使用.py
+++ 8-os模块使用.py
@@ -6,3 +6,11 @@ print(os.path.basename(__file__))
 print(os.path.realpath(__file__))
 print(os.path.dirname(__file__))
 print(os.getcwd())
+
+
+输出：
+D:\Pyup_study\Py计算机视觉\test.py
+test.py
+D:\Pyup_study\Py计算机视觉\test.py
+D:/Pyup_study/Py计算机视觉
+D:\Pyup_study\Py计算机视觉
