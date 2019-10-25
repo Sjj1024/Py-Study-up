0a31c4c5b6b75a63e5716589e01359fda9fde8df
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2019-10-25 15:22:31 +0800

    Create 34-PyQt1初始化.py

---
+++ 34-PyQt1初始化.py
@@ -0,0 +1,14 @@
+import sys
+import test
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+if __name__ == '__main__':
+    # QtWidgets是所有的控件的父类，QApplication是用来控制程序的进程，QMainWindow是控制窗口的
+    app = QApplication(sys.argv)  # 这一步是必须的，意思是创建一个app进程对象
+    mainWindow = QMainWindow()  # 这是创建窗口对象
+    ui = test.Ui_MainWindow()  # 创建test中的ui对象
+    ui.setupUi(mainWindow)  # 将窗口对象穿进去，因为这个方法需要这个窗体对象
+    mainWindow.show()
+    sys.exit(app.exec_())
+    print("niaho")
