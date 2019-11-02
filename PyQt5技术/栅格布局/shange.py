de6416a82ceb17695fa72905419a10357678c842
1024小神 <15670339118@qq.com>
2019-11-02 17:08:30 +0800

    updata

--- PyQt5技术/栅格布局/shange.py
+++ PyQt5技术/栅格布局/shange.py
@@ -1,7 +1,8 @@
 import sys
 import shan
 from PyQt5.QtWidgets import QApplication, QMainWindow
-
+# 栅格布局中尽量将各组件排放成栅格形式
+# 不然系统有时候识别不出来是栅格布局
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
