c8201bd20f8098ac140415e04a3a9e811d8a97cc
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-03-05 17:13:06 +0800

    Update 45-asyncio技术.py

--- 重要技术点/45-asyncio技术.py
+++ 重要技术点/45-asyncio技术.py
@@ -1,22 +1,29 @@
-参考文档:https://segmentfault.com/a/1190000008814676
-
-例子说明：
 import asyncio
 import time
+import functools
 
 
 async def first():
     print("nihao shijie")
     await asyncio.sleep(3)
-
+    return "first"
 
 async def second():
     print("woshi d")
     await asyncio.sleep(1)
+    return "second"
+
+async def three():
+    print("woshi disange")
+    await asyncio.sleep(1)
+    return "three"
 
 
-def call_back():
+def call_back(loop, futu):
     print("调用回调函数")
+    # 在回调函数中，同样会将future对象传进来，因此获取到一步函数的返回值
+    print(futu.result(), 111111111)
+    loop.stop()
 
 
 if __name__ == '__main__':
@@ -24,10 +31,16 @@ if __name__ == '__main__':
     # 获得时间循环对象
     loop = asyncio.get_event_loop()
     # 创建异步协成列表
-    geater = [first(), second()]
+    geater = [first(), second(), three()]
+    # 可以创建一个future对象，将多个异步任务合并成一个future
+    futu = asyncio.gather(*geater)
+    # 使用future添加异步任务都完成后的回调函数
+    # functools.partial会先将函数的部分参数传递进去，等所有参数都传递完了，再开始调用
+    futu.add_done_callback(functools.partial(call_back, loop))
     # 使用ayncio.gather将事件添加进时间循环
-    loop.run_until_complete(asyncio.gather(*geater))
+    # loop.run_until_complete(asyncio.gather(*geater))
+    loop.run_forever()
     # 循环完成后，关闭事件循环
-    loop.close()
+    # loop.close()
     ends = time.time()
     print(ends-start)
