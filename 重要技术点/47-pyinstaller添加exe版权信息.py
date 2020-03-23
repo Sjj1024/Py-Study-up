ab82eace54159a89be58dcb1053545a79af00b5e
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-03-23 15:02:09 +0800

    Update 47-pyinstaller添加exe版权信息.py

--- 重要技术点/47-pyinstaller添加exe版权信息.py
+++ 重要技术点/47-pyinstaller添加exe版权信息.py
@@ -1,2 +1,45 @@
 参考文章:
 https://www.cnblogs.com/danvy/p/10505731.html
+
+  
+生成文件后：
+# UTF-8
+#
+# For more details about fixed file info 'ffi' see:
+# http://msdn.microsoft.com/en-us/library/ms646997.aspx
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    # filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
+    # Set not needed items to zero 0.
+    filevers=(6, 3, 69, 8341),
+    prodvers=(6, 3, 69, 8341),
+    # Contains a bitmask that specifies the valid bits 'flags'r
+    mask=0x0,
+    # Contains a bitmask that specifies the Boolean attributes of the file.
+    flags=0x0,
+    # The operating system for which this file was designed.
+    # 0x4 - NT and there is no need to change it.
+    OS=0x4,
+    # The general type of file.
+    # 0x1 - the file is an application.
+    fileType=0x1,
+    # The function of the file.
+    # 0x0 - the function is not defined for this fileType
+    subtype=0x0,
+    # Creation date and time stamp.
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'080404b0',
+        [StringStruct(u'CompanyName', u'网易公司'),
+        StringStruct(u'FileDescription', u'有道词典安装程序'),
+        StringStruct(u'FileVersion', u'6.3.69.8341'),
+        StringStruct(u'LegalCopyright', u'(C) 网易公司'),
+        StringStruct(u'ProductName', u'有道词典')])
+      ]), 
+    VarFileInfo([VarStruct(u'Translation', [2052, 1200])])
+  ]
+)
