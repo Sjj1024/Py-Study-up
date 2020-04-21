0e09270b23fb22d5125cd93aebb9402ac98d1221
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-04-21 10:31:46 +0800

    Create 53-windows-docker安装问题.java

---
+++ 重要技术点/53-windows-docker安装问题.java
@@ -0,0 +1,3 @@
+Unable to stop - 已停止该运行的命令，因为首选项变量“ErrorActionPreference”或通用参数设置为 Stop: 由于以下错误无法启动服务“Hyper-V 主机计算服务 (vmcompute)”: 无法启动计算机“.”上的服务 vmcompute。
+
+右下角的工具栏，找到docker的图标，单机，然后有个【switch to windows containers...】，单机然后切换就可以了，应该是默认了Linux的container
