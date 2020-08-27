2d5ead615087c1f98d1ea909c05c261953336f6d
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-08-27 20:49:11 +0800

    Update 1-添加水印.py

--- 图片处理/1-添加水印.py
+++ 图片处理/1-添加水印.py
@@ -4,21 +4,33 @@ from PIL import Image, ImageDraw, ImageFont, ImageGrab
 # font = ImageFont.truetype('C:\Windows\Fonts\ARIALUNI.TTF', 16)
 # font = ImageFont.truetype('C:\Windows\Fonts\ARIALUNI.TTF', 16)
 # font = ImageFont.truetype('C:\Windows\Fonts\ARIALUNI.TTF', 16)
-font = ImageFont.truetype('C:\Windows\Fonts\\arialbd.ttf', 24)
+# font = ImageFont.truetype('C:\Windows\Fonts\\arialbd.ttf', 24)
+
 
 
 # image: 图片  text：要添加的文本 font：字体
-def add_text_to_image(image, text, font=font):
+def add_text_to_image(image, text):
     rgba_image = image.convert('RGBA')
     text_overlay = Image.new('RGBA', rgba_image.size, (255, 255, 255, 0))
     image_draw = ImageDraw.Draw(text_overlay)
-
+    # 设置字体大小和图片成比例
+    y = rgba_image.size[1]
+    print(y)
+    if y > 860:
+        font_size = 24
+    elif 480 < y:
+        font_size = 18
+    else:
+        font_size = 12
+    print(font_size)
+    font = ImageFont.truetype('C:\Windows\Fonts\\arialbd.ttf', font_size)
     text_size_x, text_size_y = image_draw.textsize(text, font=font)
     # 设置文本文字位置
     print(rgba_image)
-    text_xy = (rgba_image.size[0] - text_size_x - 8, rgba_image.size[1] - text_size_y - 8)
+    text_xy = (rgba_image.size[0] - text_size_x - 10, rgba_image.size[1] - text_size_y - 10)
     # 设置文本颜色和透明度
     image_draw.text(text_xy, text, font=font, fill=(240, 240, 240, 180))
+    # image_draw.text(text_xy, text, font=font, fill=(227, 62, 51, 180))
 
     image_with_text = Image.alpha_composite(rgba_image, text_overlay)
 
